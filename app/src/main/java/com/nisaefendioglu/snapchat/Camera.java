3d73d31ecacf696e56739b17303073c38bbc8b85
nisaefendioglu <nisa1517@gmail.com>
2021-08-27 22:18:35 +0300

    camera added.

--- app/src/main/java/com/nisaefendioglu/snapchat/Camera.java
+++ app/src/main/java/com/nisaefendioglu/snapchat/Camera.java
@@ -1,18 +1,44 @@
 package com.nisaefendioglu.snapchat;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import androidx.activity.result.ActivityResultLauncher;
 import androidx.fragment.app.Fragment;
 
 public class Camera  extends Fragment {
 
     View view;
+    private ImageView imageView;
+    private static final int CAMERA_REQUEST=0;
 
     @Override
-    public View onCreateView( LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        view = inflater.inflate(R.layout.camera,container,false);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.camera, container, false);
+
+        imageView = view.findViewById(R.id.imageView);
+
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        getActivity().startActivityForResult(intent, CAMERA_REQUEST);
         return view;
     }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == CAMERA_REQUEST && resultCode == Activity.RESULT_OK)
+        {
+            Bitmap image = (Bitmap) data.getExtras().get("data");
+            imageView.setImageBitmap(image);
+        }
+    }
+
 }
