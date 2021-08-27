0f670bc3e7afb5eb23fca3b4c0a9ac10be3738d5
nisaefendioglu <nisa1517@gmail.com>
2021-08-27 13:56:45 +0300

    chat page added.

--- app/src/main/java/com/nisaefendioglu/snapchat/Chat.java
+++ app/src/main/java/com/nisaefendioglu/snapchat/Chat.java
@@ -4,15 +4,51 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 
 import androidx.fragment.app.Fragment;
 
+import java.util.ArrayList;
+
 public class Chat extends Fragment {
     View view;
+    ListView chatList;
+    final ArrayList<ChatList> chatListArrayList = new ArrayList<>();
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        view = inflater.inflate(R.layout.chat, container, false);
+
+        view = inflater.inflate(R.layout.chatlist, container, false);
+
+        chatList = view.findViewById(R.id.chatList);
+        chatListArrayList.add(new ChatList("Nisa","Tap to chat!",R.drawable.avatar8));
+        chatListArrayList.add(new ChatList("Mehmet","Tap to chat!",R.drawable.avatar9));
+        chatListArrayList.add(new ChatList("Halise","Tap to chat!",R.drawable.avatar));
+        chatListArrayList.add(new ChatList("Hülya","Tap to chat!",R.drawable.avatar3));
+        chatListArrayList.add(new ChatList("Ahmet","Tap to chat!",R.drawable.avatar6));
+        chatListArrayList.add(new ChatList("Çağla","Tap to chat!",R.drawable.avatar4));
+        chatListArrayList.add(new ChatList("Pelin","Tap to chat!",R.drawable.avatar5));
+        chatListArrayList.add(new ChatList("Murat","Tap to chat!",R.drawable.avatar2));
+        chatListArrayList.add(new ChatList("Aslı","Tap to chat!",R.drawable.avatar));
+        chatListArrayList.add(new ChatList("Nisa","Tap to chat!",R.drawable.avatar8));
+        chatListArrayList.add(new ChatList("Mehmet","Tap to chat!",R.drawable.avatar9));
+        chatListArrayList.add(new ChatList("Halise","Tap to chat!",R.drawable.avatar));
+        chatListArrayList.add(new ChatList("Hülya","Tap to chat!",R.drawable.avatar3));
+        chatListArrayList.add(new ChatList("Ahmet","Tap to chat!",R.drawable.avatar6));
+        chatListArrayList.add(new ChatList("Çağla","Tap to chat!",R.drawable.avatar4));
+        chatListArrayList.add(new ChatList("Pelin","Tap to chat!",R.drawable.avatar5));
+        chatListArrayList.add(new ChatList("Murat","Tap to chat!",R.drawable.avatar2));
+        chatListArrayList.add(new ChatList("Aslı","Tap to chat!",R.drawable.avatar));
+
+        ChatAdapter mailAdapter = new ChatAdapter(getActivity(), chatListArrayList);
+        if (chatList != null) {
+            chatList.setAdapter(mailAdapter);
+        }
+
         return view;
+
+
     }
+
+
 }
--- app/src/main/java/com/nisaefendioglu/snapchat/MainActivity.java
+++ app/src/main/java/com/nisaefendioglu/snapchat/MainActivity.java
@@ -1,14 +1,20 @@
 package com.nisaefendioglu.snapchat;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
+import android.content.Intent;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.widget.ListView;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
     BottomNavigationView bottomNavigationView;
